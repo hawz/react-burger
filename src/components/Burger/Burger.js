eea8d6542398189210ce7251fa7b40fc19570cb0
Alberto Masia <alberto.masia@gmail.com>
2019-06-27 15:05:40 +0200

    Display ingredients from state.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  // first we transform the ingredient object into an array of ingredients
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((value, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    }).reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+    if(transformedIngredients.length === 0) {
+      transformedIngredients = <p>Your burger is empty. <br /> Start adding ingredients!</p>
+    }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
