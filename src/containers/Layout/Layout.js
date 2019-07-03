2098b5f20079fcf0e059fba2f83579e529e69d27
Alberto Masia <alberto.masia@gmail.com>
2019-07-03 10:49:59 +0200

    Move Layout component from components to containers

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import classes from './Layout.module.css';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
+
+  sideDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true })
+  }
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar open={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          showDrawer={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
