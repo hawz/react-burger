4ffae9e473a0aa64233ba3873955f23ae160b059
Alberto Masia <alberto.masia@gmail.com>
2019-07-08 16:22:36 +0200

    Insert Checkout component into App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React, { Component } from 'react';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
-  // state = {
-  //   show: true
-  // }
-
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({ show: false })
-  //   }, 3000)
-  // }
-
   render() {
     return (
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div >
     );
