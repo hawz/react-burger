8b6b5e5462ba052aae5ccc829f30983139f607b5
Alberto Masia <alberto.masia@gmail.com>
2019-07-09 12:57:16 +0200

    Redirect to checkout component in BurgerBuilder.
    Instead of handling the order here, making the API post call, we just redirect to the checkout component.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -106,32 +106,20 @@ class BurgerBuilder extends Component {
     // alert('You continue!');
     // no checkout for now (we need routes and form validation features we don't have yet)
     // for Firebase to function correctly, we need to add '.json' to the endpoint we're sending the request to.
-    this.setState({ loading: true });
+    // this.setState({ loading: true });
 
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Albe Masia',
-        address: {
-          street: 'TestStreet 1',
-          zipCode: '12345',
-          country: 'Italy'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
-    };
 
-    axios.post('/orders.json', order)
-      .then(response => {
-        // console.log(response);
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-        console.log(error);
-      })
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+    }
+    queryParams.push('price='+this.state.totalPrice);
+    const queryString = queryParams.join('&');
+
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   }
 
   resetBurgerHandler = () => {
