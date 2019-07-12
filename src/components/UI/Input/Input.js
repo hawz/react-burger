06d8ebf22b6bf42ccb37f870c772b46488faaf13
Alberto Masia <alberto.masia@gmail.com>
2019-07-12 10:55:04 +0200

    Add validation features to input component.

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,18 +4,28 @@ import classes from './Input.module.css';
 
 const input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
+
+  let validationError = null;
+  if (props.invalid && props.touched) {
+    validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>;
+  }
 
   switch (props.elementType) {
     case ('input'):
       inputElement = <input
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.changed} />
       break;
     case ('textarea'):
       inputElement = <textarea
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.changed} />
@@ -23,7 +33,7 @@ const input = (props) => {
     case ('select'):
       inputElement = (
         <select
-          className={classes.InputElement}
+          className={inputClasses.join(' ')}
           value={props.value}
           onChange={props.changed}>
           {props.elementConfig.options.map(option => (
@@ -34,7 +44,7 @@ const input = (props) => {
       break;
     default:
       inputElement = <input
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         {...props.elementConfig}
         value={props.value}
         onChange={props.changed} />
@@ -45,6 +55,7 @@ const input = (props) => {
     <div className={classes.Input}>
       <label className={classes.Label}>{props.label}</label>
       {inputElement}
+      {validationError}
     </div>
   );
 
