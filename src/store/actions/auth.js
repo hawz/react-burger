d72ad5c0e69aad0a057cac3eadacea6afa48ebb5
Alberto Masia <alberto.masia@gmail.com>
2019-07-19 12:14:18 +0200

    Create action creators for auth actions.

---
+++ src/store/actions/auth.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+import { FIREBASE_API_KEY } from '../../config';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = (token, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error
+  };
+};
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    // ... axios stuff
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
+    if (!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword'
+    }
+
+    axios.post(`${url}?key=${FIREBASE_API_KEY}`, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(authFail(error));
+      })
+  }
+}
