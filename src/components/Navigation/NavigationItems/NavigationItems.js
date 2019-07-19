882a902fbf806e6f450f20d6f0b82df7f92274f7
Alberto Masia <alberto.masia@gmail.com>
2019-07-19 15:57:06 +0200

    Render different buttons according to auth state.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,10 @@ const navigationItems = (props) => (
     <ul>
       <NavigationItem link="/" exact>Burger Builder</NavigationItem>
       <NavigationItem link="/orders">Orders</NavigationItem>
-      <NavigationItem link="/auth">Authenticate</NavigationItem>
+      {props.isAuthenticated
+        ? <NavigationItem link="/logout">Logout</NavigationItem>
+        : <NavigationItem link="/auth">Authenticate</NavigationItem>
+      }
     </ul>
   </nav>
 );
