c65b2108fd3a4876147da378bf1e20549c8092c4
Alberto Masia <alberto.masia@gmail.com>
2019-07-19 12:12:39 +0200

    Add specific route to auth.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import OrderDetails from './containers/Order/Order';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" exact component={Orders} />
             <Route path="/orders/:id" exact component={OrderDetails} />
+            <Route path="/auth" exact component={Auth} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
