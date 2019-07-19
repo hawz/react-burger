18d26e88e074d33cd4cd3521af89edfac66ea298
Alberto Masia <alberto.masia@gmail.com>
2019-07-19 12:14:30 +0200

    Export auth action.

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,13 +1,21 @@
+// Actions exported from burgerBuilder.js action creators file
 export {
   addIngredient,
   removeIngredient,
   resetIngredients,
   initIngredients
 } from './burgerBuilder';
+
+// Actions exported from order.js action creators file
 export {
   purchaseBurger,
   purchaseInit,
   fetchOrders,
   fetchOrder,
-  deleteOrder
+  deleteOrder,
 } from './order';
+
+// Actions exported from auth.js action creators file
+export {
+  auth
+} from './auth';
