ec11a4818c215e4dae3fa7d7207011df624b292a
Alberto Masia <alberto.masia@gmail.com>
2019-07-19 12:48:05 +0200

    Add authLogout to reducer.

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -20,12 +20,19 @@ const authSuccess = (state, action) => {
     error: null,
     loading: false
   });
-}
+};
 
 const authFail = (state, action) => {
   return updateObject(state, {
     error: action.error,
     loading: false
+  })
+};
+
+const authLogout = (state, action) => {
+  return updateObject(state, {
+    token: null,
+    userId: null
   })
 }
 
@@ -34,6 +41,7 @@ const reducer = (state = initialState, action) => {
     case actionTypes.AUTH_START: return authStart(state, action);
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
     case actionTypes.AUTH_FAIL: return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
     default: return state;
   }
 }
