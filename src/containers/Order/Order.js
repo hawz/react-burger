976b991df591a422fe2bf1f751a8e7d39a30eb6c
Alberto Masia <alberto.masia@gmail.com>
2019-09-20 12:57:51 +0200

    Use hooks in Order component

--- src/containers/Order/Order.js
+++ src/containers/Order/Order.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux'
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
@@ -8,63 +8,63 @@ import classes from './Order.module.css';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import * as actions from '../../store/actions/index';
 
-class OrderDetails extends Component {
-  componentDidMount() {
-    this.props.onFetchSelectedOrder(this.props.match.params.id, this.props.token);
-  }
+const OrderDetails = props => {
+  useEffect(() => {
+    props.onFetchSelectedOrder(props.match.params.id, props.token);
+  }, []);
 
-  render() {
-    let order = <Spinner />;
-    if (!this.props.loading && this.props.selectedOrder) {
 
-      const ingredients = [];
-      for (let ingredientName in this.props.selectedOrder.ingredients) {
-        ingredients.push({ name: ingredientName, amount: this.props.selectedOrder.ingredients[ingredientName] })
-      }
+  let order = <Spinner />;
+  if (!props.loading && props.selectedOrder) {
 
-      const orderDetails = [];
-      for (let detailName in this.props.selectedOrder.orderData) {
-        orderDetails.push({ name: detailName, value: this.props.selectedOrder.orderData[detailName] });
-      }
-
-      const ingredientOutput = ingredients.map(ig => {
-        return (
-          <span
-            key={ig.name}
-            className={classes.Ingredient}
-          >
-            {ig.name} ({ig.amount})
-          </span>
-        );
-      });
-
-      const orderDetailsOutput = orderDetails.map(od => (
-        <Aux key={od.name}>
-          <dt>{od.name}</dt>
-          <dd>{od.value}</dd>
-        </Aux>
-      ));
-
-      order = (
-        <Aux>
-          <h1>Your burger</h1>
-          <p>Price: <strong>{this.props.selectedOrder.price.toFixed(2)} €</strong></p>
-          <hr />
-          <p>Ingredients: {ingredientOutput}</p>
-          <hr />
-          <p>More details:</p>
-          <dl className={classes.DetailsList}>
-            {orderDetailsOutput}
-          </dl>
-        </Aux>
-      );
+    const ingredients = [];
+    for (let ingredientName in props.selectedOrder.ingredients) {
+      ingredients.push({ name: ingredientName, amount: props.selectedOrder.ingredients[ingredientName] })
     }
-    return (
-      <div className={classes.Order}>
-        {order}
-      </div>
-    )
+
+    const orderDetails = [];
+    for (let detailName in props.selectedOrder.orderData) {
+      orderDetails.push({ name: detailName, value: props.selectedOrder.orderData[detailName] });
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+      return (
+        <span
+          key={ig.name}
+          className={classes.Ingredient}
+        >
+          {ig.name} ({ig.amount})
+          </span>
+      );
+    });
+
+    const orderDetailsOutput = orderDetails.map(od => (
+      <Aux key={od.name}>
+        <dt>{od.name}</dt>
+        <dd>{od.value}</dd>
+      </Aux>
+    ));
+
+    order = (
+      <Aux>
+        <h1>Your burger</h1>
+        <p>Price: <strong>{props.selectedOrder.price.toFixed(2)} €</strong></p>
+        <hr />
+        <p>Ingredients: {ingredientOutput}</p>
+        <hr />
+        <p>More details:</p>
+        <dl className={classes.DetailsList}>
+          {orderDetailsOutput}
+        </dl>
+      </Aux>
+    );
   }
+  return (
+    <div className={classes.Order}>
+      {order}
+    </div>
+  )
+
 }
 
 const mapStateToProps = state => {
