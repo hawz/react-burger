60bd5ef985e0fe915d704e1bc48a0bcba303f418
Alberto Masia <alberto.masia@gmail.com>
2019-09-20 12:30:46 +0200

    Adjust App component to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -12,14 +12,39 @@ const Orders = React.lazy(() => import('./containers/Orders/Orders'));
 const Auth = React.lazy(() => import('./containers/Auth/Auth'));
 const Logout = React.lazy(() => import('./containers/Auth/Logout/Logout'));
 
-class App extends Component {
-  componentWillMount() {
-    this.props.onTryAutoSignup();
-  }
+const App = props => {
+  useEffect(() => {
+    console.log('called hook');
+    props.onTryAutoSignup();
+  }, [props]);
 
-  render() {
-    let routes = (
+  let routes = (
+    <Switch>
+      <Route path="/auth" render={() => <Auth />} />
+      <Route path="/" exact component={BurgerBuilder} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
+  if (props.isAuthenticated) {
+    routes = (
       <Switch>
+        <Route path="/checkout" render={() => (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Checkout />
+          </Suspense>
+        )} />
+        <Route path="/orders" exact render={() => (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Orders />
+          </Suspense>
+        )} />
+        <Route path="/orders/:id" exact component={OrderDetails} />
+        <Route path="/logout" render={() => (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Logout />
+          </Suspense>
+        )} />
         <Route path="/auth" render={() => (
           <Suspense fallback={<div>Loading...</div>}>
             <Auth />
@@ -29,45 +54,15 @@ class App extends Component {
         <Redirect to="/" />
       </Switch>
     );
-
-    if (this.props.isAuthenticated) {
-      routes = (
-        <Switch>
-          <Route path="/checkout" render={() => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <Checkout />
-            </Suspense>
-          )} />
-          <Route path="/orders" exact render={() => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <Orders />
-            </Suspense>
-          )} />
-          <Route path="/orders/:id" exact component={OrderDetails} />
-          <Route path="/logout" render={() => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <Logout />
-            </Suspense>
-          )} />
-          <Route path="/auth" render={() => (
-            <Suspense fallback={<div>Loading...</div>}>
-              <Auth />
-            </Suspense>
-          )} />
-          <Route path="/" exact component={BurgerBuilder} />
-          <Redirect to="/" />
-        </Switch>
-      );
-    }
-
-    return (
-      <div>
-        <Layout>
-          {routes}
-        </Layout>
-      </div >
-    );
   }
+
+  return (
+    <div>
+      <Layout>
+        {routes}
+      </Layout>
+    </div >
+  );
 }
 
 const mapStateToProps = state => {
@@ -82,4 +77,9 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App)
+);
